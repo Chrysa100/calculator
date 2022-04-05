ccef95dd807c82a52749a638f80c2eaf3efc6108
Chrysa <chrysapa@gmail.com>
2022-04-05 17:56:49 +0300

    start with the basic math  functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    console.log(a+b);
+    return a+b;
+}
+function subtract(a,b){
+    console.log(a-b);
+    return a-b;
+}function multiply(a,b){
+    console.log(a*b);
+    return a*b;
+}
+function divide(a,b){
+    console.log(a/b);
+    return a/b;
+}
+
+add(5,3);
+subtract(3-5);
+multiply(2*5);
+divide(5/4);
