3b141aa987cb7daaced1f0d8a84bb50dd8db4858
Chrysa <chrysapa@gmail.com>
2022-04-07 23:43:02 +0300

    add AC & multiple numbers

--- script.js
+++ script.js
@@ -7,52 +7,50 @@ let num1 = "";
 let num2 = "";
 let operator = "";
 let result = "";
-
-//initialization of innerText
-display.innerText = num1;
+let action = "";
 
 //add EventListeners to buttons
 buttons.forEach(btn => btn.addEventListener("click",toDisplay));
 
 //function that runs every time a button is clicked
 function toDisplay(btn) {
-    
-    if(this.innerText >= 0 && this.innerText <= 9){
-        if(operator ===""){     
-            num1 +=  this.innerText; 
-            display.innerText = num1;
-            console.log ("anum1 " +num1);
-            console.log ("anum2 " +num2);
+    if(this.innerText === "AC"){
+        display.innerText = "";
+    }else {
+        if(this.innerText >= 0 && this.innerText <= 9){
             
-        }else{
+            if(operator === ""){     
+                num1 +=  this.innerText; 
+                display.innerText = num1;
+            }else{
             num2 += this.innerText;
-            display.innerText += num2;
-            console.log ("bnum1 " +num1);
-            console.log ("bnum2 " +num2);        
-            
+            display.innerText = num2;
+            }
+    
+        }else if(this.innerText.match(/[+,*,/,=,-]/)){
+            if(operator ===""){
+                operator =  this.innerText;
+                display.innerText = operator;
+            }else{
+                action =  this.innerText;
+                display.innerText = action;
+            }console.log ("G action " +action);
+
+            if(num2 !==""){
+                operate(operator,num1*1,num2*1);
+                display.innerText = result;
+                num1 = result.toString();
+                num2.innerText = "";
+                result.innerText = "";
+                operator = action;
+                action ="";
+                //operator.innerText = "";
+            }  
         }
-    }else if(this.innerText.match(/[+,*,/,-]/)){
-        operator =  this.innerText;
-        display.innerText += operator;
-
-    }else if(this.innerText === "="){
-        console.log ("onum1 " +num1);
-        console.log ("onum2 " +num2);
-        display.innerText += this.innerText;
-        operate(operator,num1*1,num2*1);
-        display.innerText = result;
-        num1 = result.toString();
-        num2="";
-        console.log ("num1 " +num1);
-        console.log ("num2 " +num2);
-        console.log(typeof(num1));
-        console.log(typeof(num2));
-        
-       
-        //return num1;
-    } 
-}
-
+    }
+      }
+    
+ 
 function add(a,b){
     result = a+b;
     return result;
@@ -79,10 +77,16 @@ function operate(operator,a,b){
         case "*" : multiply(a,b);
         break;
         case "/" : divide(a,b);
-        }  
+        }
+        num1="";
+        num2="";
+        console.log("operate 1 " +num1);
+        console.log("operate 2 " +num2);
         return result;             
 }
 
 
 
+/* 
 
+ */
